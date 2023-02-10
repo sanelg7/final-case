417af7dbe15b797a55d6135221f56912072602ca
Ege Şanel <sanelege@gmail.com>
2023-02-10 23:50:55 +0300

    Created basic user class and roles. Also configured the db connection.

--- src/main/java/com/definex/practicum/finalcase/config/DatabaseConfig.java
+++ src/main/java/com/definex/practicum/finalcase/config/DatabaseConfig.java
@@ -1,2 +1,44 @@
-package com.definex.practicum.finalcase.config;public class DatabaseConfig {
+package com.definex.practicum.finalcase.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@PropertySource("classpath:persistence-mysql.properties")
+public class DatabaseConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        }
+        // TODO : Handle PropertyVetoException
+        catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUser(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        dataSource.setInitialPoolSize(Integer.parseInt(environment.getProperty("connection.pool.initialPoolSize")));
+        dataSource.setMinPoolSize(Integer.parseInt(environment.getProperty("connection.pool.minPoolSize")));
+        dataSource.setMaxPoolSize(Integer.parseInt(environment.getProperty("connection.pool.maxPoolSize")));
+        dataSource.setMaxIdleTime(Integer.parseInt(environment.getProperty("connection.pool.maxIdleTime")));
+
+        return dataSource;
+
+    }
 }
--- src/main/java/com/definex/practicum/finalcase/model/Role.java
+++ src/main/java/com/definex/practicum/finalcase/model/Role.java
@@ -1,2 +1,12 @@
-package com.definex.practicum.finalcase.model;public class Role {
+package com.definex.practicum.finalcase.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public enum Role {
+
+    ADMIN,
+    NORMAL
 }
