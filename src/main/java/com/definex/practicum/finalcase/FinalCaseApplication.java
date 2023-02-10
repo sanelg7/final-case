98b53a956e20c05393b174fcbae7e91b67d6d95a
Ege Şanel <sanelege@gmail.com>
2023-02-10 18:38:53 +0300

    Initialized project using spring initializr

---
+++ src/main/java/com/definex/practicum/finalcase/FinalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.definex.practicum.finalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/definex/practicum/finalcase/FinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definex.practicum.finalcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
